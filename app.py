54121925d63cf68f3e2d631fdb4cc196125f1963
ishantgupta1608 <ishantgupta1608@gmail.com>
2020-03-27 12:34:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    connect_str = 'DefaultEndpointsProtocol=https;AccountName=allmyfiles;AccountKey=6kHeRxzeEIwkywZ5gv4dKC9/aLdiQjZqfS+VbBukijNTZin7kLDjfrbNDd4Y8J05s6rXJ/TiQYvop0J5DzJCpg==;EndpointSuffix=core.windows.net'
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
     #container_client = blob_service_client.create_container('secondcontainer')
